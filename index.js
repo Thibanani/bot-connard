b88360b89d7549d063e828828403c89cb3da74a1
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-09 23:55:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 
 bot.on('ready', async() =>{
-  bot.user.setActivity('bot connard. Québecqwé')
+  bot.user.setActivity('Québecqwé')
 });
 
 bot.login(process.env.BOT_TOKEN);
