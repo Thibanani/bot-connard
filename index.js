890e6c2563ab1bc7c2eb45e29ce11d0d2cd8e3ba
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-10 00:24:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ bot.on('message', async (msg) =>{
   if(msg.content == 'jtm'){
     msg.channel.send('Connard!')
   }
+  if(msg.content.startsWith(config.prefix)){
+    cmdArray = msg.content.split()
+    cmd = cmdArray[0]
+    args = cmdArray.slice(1)
+
+    msg.channel.send('Seules les dieux me donne des ordres. Connard!')
+  }
 });
 
 bot.login(config.token);
