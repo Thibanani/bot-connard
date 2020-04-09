e830338556e9e2bb80b47b24dbcc035290143877
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-10 01:45:58 +0200

    Update VoiceConnection.js

--- commands/VoiceConnection.js
+++ commands/VoiceConnection.js
@@ -1,5 +1,9 @@
 module.exports.run = async (bot, msg, args)=> {
-
+  if (msg.member.voice.channel) {
+    const connection = await msg.member.voice.channel.join();
+  } else {
+    msg.reply('You need to join a voice channel first!');
+  }
 }
 
 module.exports.help = {
