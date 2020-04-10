287fcc51d711b05b1daf61e3e3cc97f58fb7f7e7
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-10 02:06:05 +0200

    test

--- commands/VoiceConnection.js
+++ commands/VoiceConnection.js
@@ -1,25 +1,6 @@
 module.exports.run = async (bot, msg, args)=> {
-  if (msg.member.voice.channel) {
 
-    const connection = await msg.member.voice.channel.join();
-    const dispatcher = connection.play('./audio.mp3');
-
-    dispatcher.pause();
-    dispatcher.resume();
-
-    dispatcher.setVolume(0.5); // half the volume
-
-    dispatcher.on('finish', () => {
-      console.log('Finished playing!');
-    });
-
-    dispatcher.destroy(); // end the stream
-
-  } else {
-    msg.reply('You need to join a voice channel first!');
-  }
-}
 
 module.exports.help = {
-  name: 'viens'
+  name: 'join'
 }
--- index.js
+++ index.js
@@ -23,6 +23,30 @@ bot.on('message', async (msg) =>{
     let command = commands.getCommand(cmd);
     if(command) command.run(bot, msg, args)
 
+    if (cmd === 'viens') {
+      if (msg.member.voice.channel) {
+
+        const connection = await msg.member.voice.channel.join();
+        const dispatcher = connection.play('./audio.mp3');
+
+        dispatcher.pause();
+        dispatcher.resume();
+
+        dispatcher.setVolume(0.5); // half the volume
+
+        dispatcher.on('finish', () => {
+          console.log('Finished playing!');
+        });
+
+        dispatcher.destroy(); // end the stream
+
+      } else {
+        msg.reply('You need to join a voice channel first!');
+      }
+    }
+
+    }
+
   }
 });
 
