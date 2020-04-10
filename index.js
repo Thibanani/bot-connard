6c6de19fa3a3c34777e66fe59732364edb8bf087
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-10 03:03:44 +0200

    return to V1 version

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on('message', async (msg) =>{
     //let command = commands.getCommand(cmd);
     //if(command) command.run(bot, msg, args)
 
-      if (cmd.content === 'viens') {
+      if (cmd === 'viens') {
           if (msg.member.voice.channel) {
             const connection = await msg.member.voice.channel.join();
 
