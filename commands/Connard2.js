75402834d7df69d6d33bc73f59f251a42deac785
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-14 03:39:07 +0200

    Create Connard2.js

---
+++ commands/Connard2.js
@@ -0,0 +1,18 @@
+module.exports.run = async (bot, msg, args)=> {
+  if (msg.member.voice.channel) {
+
+    const connection = await msg.member.voice.channel.join();
+    connection.play('./audio.mp3', {volume: 2});
+    connection.play('./audio.mp3', pause());
+    connection.play('./audio.mp3', resume());
+
+    voiceChannel.leave();
+  } else {
+    msg.reply('Seules les dieux me donne des ordres. Connard!');
+  }
+}
+
+
+module.exports.help = {
+  name: 'p'
+}
