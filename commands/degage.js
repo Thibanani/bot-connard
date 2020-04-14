b8ec54be260fa2f3a27a704bc786d96144154741
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-14 04:27:20 +0200

    Create degage.js

---
+++ commands/degage.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+module.exports.run = async (bot, msg, args)=> {
+  if (msg.member.voice.channel) {
+
+    const connection = await msg.member.voice.channel.join();
+    const dispatcher = connection.play('./audio.mp3');
+    dispatcher.destroy();
+    connection.disconnect();
+
+  } else {
+    msg.reply('Seules les dieux me donne des ordres. Connard!');
+  }
+
+
+}
+
+
+module.exports.help = {
+  name: 'parle'
+}
