458a513e814bdfaa9a7796a91148ec08ba7a8642
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-15 17:25:34 +0200

    Update PFC.js

--- commands/PFC.js
+++ commands/PFC.js
@@ -4,26 +4,27 @@ module.exports.run = async (bot,msg,args) => {
   const user = msg.author.id
   const filter = m => user === m.author.id;
   const collector = msg.channel.createMessageCollector(filter, { max: 1,time: 15000 });
-  //console.log(`Collected2 ${collector.on('collect',m)}`);
 
-  console.log(`collected ${collector.on('collect', m => {m.content})}`);
+  collector.on('collect', m => {
+  	console.log(`Collected ${m.content}`);
 
-  //const rep = collector.on('collect', m => {m.content});
-  //console.log(`Collected2 ${collector.on('collect', m.content)}`);
+    if (m.content == ':fist:'){
+      msg.channel.send(`:raised_hand:`)
+      msg.channel.send(`Bas alors on est nul, tocard`)
 
-  /*if (m.content == ':fist:'){
-    msg.channel.send(`:raised_hand:`)
-    msg.channel.send(`Bas alors on est nul, tocard`)
+    }else if (m.content == ':raised_hand:') {
+      msg.channel.send(`:v:`)
+      msg.channel.send(`Bas alors on est nul, tocard`)
 
-  }else if (m.content == ':raised_hand:') {
-    msg.channel.send(`:v:`)
-    msg.channel.send(`Bas alors on est nul, tocard`)
+    }else if (m.content == ':v:') {
+      msg.channel.send(`:fist:`)
+      msg.channel.send(`Bas alors on est nul, tocard`)
 
-  }else if (m.content == ':v:') {
-    msg.channel.send(`:fist:`)
-    msg.channel.send(`Bas alors on est nul, tocard`)
+    }else{msg.channel.send(`Sale couard !`)}
+  });
 
-  }else{msg.channel.send(`Sale couard !`)}*/
+
+
 
 
 }
