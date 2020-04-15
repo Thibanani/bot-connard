ae077a41a6f80078f4fb9a53f6ae0d9e10684269
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-15 15:23:36 +0200

    Create PFC.js

---
+++ commands/PFC.js
@@ -0,0 +1,10 @@
+module.exports.run = async (bot,msg,args) => {
+  msg.channel.send(`Ha ouai, tu veux qu'on se règle`)
+  msg.channel.send(`On rappel pour les débiles : pierre:fist: ; feuille:raised_hand: ; ciseaux:v:`)
+
+
+}
+
+module.exports.help = {
+  name: 'PFC'
+}
