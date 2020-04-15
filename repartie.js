bc5475dd1cd2e152e2fce998af5d77dc1968f820
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-15 19:51:14 +0200

    changement

--- repartie.js
+++ repartie.js
@@ -1,4 +1,9 @@
 const repartie = () => {
-  const pfc1 = [`Ha ouai, tu veux qu'on se règle`,`Viens chte bz`];
 
 }
+
+
+  const getpfc1 = (num) => {
+    const pfc1 = [`Ha ouai, tu veux qu'on se règle`,`Viens chte bz`];
+    return pfc1[num]
+  };
