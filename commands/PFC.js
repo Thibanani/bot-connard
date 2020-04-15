139f688268646e3d0c6c038626b02e646ca8deb2
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-15 16:24:32 +0200

    Update PFC.js

--- commands/PFC.js
+++ commands/PFC.js
@@ -1,9 +1,9 @@
 module.exports.run = async (bot,msg,args) => {
   msg.channel.send(`Ha ouai, tu veux qu'on se règle`)
   msg.channel.send(`On rappel pour les débiles : pierre:fist:; feuille:raised_hand:; ciseaux:v:`)
+  const joueur = msg.author.username
 
-
-  const filter = m => msg.author.username;
+  const filter = m => m.author.joueur;
   const collector = msg.channel.createMessageCollector(filter, { time: 15000 });
 
   collector.on('collect', m => {
