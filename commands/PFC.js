0cccd100b01a87a0ae1ff709b1850d500be595f9
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-15 16:10:27 +0200

    Update PFC.js

--- commands/PFC.js
+++ commands/PFC.js
@@ -2,16 +2,12 @@ module.exports.run = async (bot,msg,args) => {
   msg.channel.send(`Ha ouai, tu veux qu'on se règle`)
   msg.channel.send(`On rappel pour les débiles : pierre:fist:; feuille:raised_hand:; ciseaux:v:`)
 
-  const filter = m => m.content.includes('discord');
-  const collector = msg.channel.createMessageCollector(filter, { time: 15000 });
-
-  collector.on('collect', m => {
-  	console.log(`Collected ${m.content}`);
-  });
-
-  collector.on('end', collected => {
-  	console.log(`Collected ${collected.size} items`);
-  });
+  // Await !vote messages
+  const filter = m => msg.author.username;
+  // Errors: ['time'] treats ending because of the time limit as an error
+  channel.awaitMessages(filter, { max: 4, time: 60000, errors: ['time'] })
+    .then(collected => console.log(collected.size))
+    .catch(collected => console.log(`After a minute,  ${collected.content}.`));
 
 }
 
