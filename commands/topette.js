1160f61f59b1df58def274e304378343c70e53cd
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-17 02:12:56 +0200

    add topette

---
+++ commands/topette.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+module.exports.run = async (bot, msg, args)=> {
+  if (msg.member.voice.channel) {
+
+    const connection = await msg.member.voice.channel.join();
+    const dispatcher = connection.play('./topette.mp3');//, {volume: 2});
+
+    dispatcher.setVolume(2);
+
+     dispatcher.on('finish', () => {
+       connection.disconnect();
+    });
+
+  } else {
+    msg.reply('Seules les dieux me donne des ordres. Connard!');
+  }
+
+
+}
+
+
+module.exports.help = {
+  name: 'topette'
+}
