f43fe4c8f299a10d4ecfa5ebcf25903a4a0858d2
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-18 14:33:14 +0200

    Update Connard.js

--- commands/Connard.js
+++ commands/Connard.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports.run = async (bot, msg, args)=> {
   if (args != ''){
     user = args
-    if (msg.user.voice == args) {
+    if (msg.users.voice.channel == args) {
 
       const connection = await msg.member.voice.channel.join();
       const dispatcher = connection.play('./connard.mp3');//, {volume: 2});
