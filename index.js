ff5d79bd74a08e348d7f8f73a076fc348141e0c1
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-23 17:03:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const config = require('./config.js');
 const commands = require('./commands.js');
 
 const bonjour = ["Salut beau gosse","Ouai, salut ouai","Yo","Bonjour à toi","HOOOO, sa gambit ou quoi ?","jtm bb"]
+const bonsoir = ["Bonsoir","Bonsoir, jeune gueux","Comment von-je ?"]
 
 bot.on('ready', async() =>{
   console.log('Le bot est lancé.');
-  bot.user.setActivity('Québecqwé')
+  bot.user.setActivity('Québecqwé')S<
 });
 
 bot.on('message', async (msg) =>{
-  if((msg.content == 'Bonjour')||(msg.content == 'Salut')){
+  if((msg.content == 'Bonjour')||(msg.content == 'Salut'){
     msg.channel.send(bonjour[Math.floor(Math.random() * (bonjour.length - 0)+ 0)])
+  }
+  if(msg.content == 'Bonsoir'){
+    msg.channel.send(`${www.tenor.com/view/penguin-hello-hi-heythere-cutie-gif-3950966}`)
   }
 
   if(msg.content.startsWith(config.prefix)){
