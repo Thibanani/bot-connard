ecfe3caf6459df3aa164ada39fa223c48e6ffa50
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-04-25 00:45:37 +0200

    Update PDF.js

--- commands/PDF.js
+++ commands/PDF.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot,msg,args) => {
-  const respirez = require('./Respirez.pdf');
+  //const respirez = require('./Respirez.pdf');
   msg.channel.send(`Respirez sinon vous allez mourir !`)
-  msg.channel.send(./Respirez.pdf)
+  //msg.channel.send(./Respirez.pdf)
 }
 
 module.exports.help = {
