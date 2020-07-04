aa9b90f6ffacc8ce3c272451ea6917ea8c67a845
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-07-04 18:33:29 +0200

    AQSV
    A quoi servez vous

---
+++ commands/AQSV.js
@@ -0,0 +1,30 @@
+module.exports.run = async (bot, msg, args)=> {
+  if (msg.member.voice.channel) {
+
+    const connection = await msg.member.voice.channel.join();
+    const dispatcher = connection.play('./AQSV.mp3');//, {volume: 2});
+
+    dispatcher.setVolume(2);
+
+     dispatcher.on('finish', () => {
+       delay(1000);
+
+       const dispatcher = connection.play('./AQSV_2.mp3');//, {volume: 2});
+       dispatcher.setVolume(2);
+
+        dispatcher.on('finish', () => {
+          connection.disconnect();
+       });
+    });
+
+
+
+  } else {
+    msg.reply('Seules les dieux me donne des ordres!');
+  }
+}
+
+
+module.exports.help = {
+  name: 'QR'
+}
