f577271c2edc2c61d74a43d6cc804a2115a53785
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-07-04 19:06:37 +0200

    Update AQSV.js

--- commands/AQSV.js
+++ commands/AQSV.js
@@ -2,21 +2,20 @@ module.exports.run = async (bot, msg, args)=> {
   if (msg.member.voice.channel) {
 
     const connection = await msg.member.voice.channel.join();
-    const dispatcher = connection.play('./AQSV.mp3');//, {volume: 2});
+    const dispatcher = connection.play('./AQSV_1.mp3');//, {volume: 2});
 
     dispatcher.setVolume(2);
 
      dispatcher.on('finish', () => {
-
-       const dispatcher = connection.play('./AQSV_2.mp3');//, {volume: 2});
-       dispatcher.setVolume(2);
-
-        dispatcher.on('finish', () => {
-          connection.disconnect();
-       });
     });
 
+    const dispatcher = connection.play('./AQSV_2.mp3');//, {volume: 2});
 
+    dispatcher.setVolume(2);
+
+     dispatcher.on('finish', () => {
+       connection.disconnect();
+    });
 
   } else {
     msg.reply('Seules les dieux me donne des ordres!');
