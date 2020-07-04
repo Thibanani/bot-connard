90e7ad31508bf8e7c1e9c5eca82b0c56b2d01b3d
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-07-04 19:10:59 +0200

    AQVS les deux

--- commands/AQSV.js
+++ commands/AQSV.js
@@ -2,14 +2,7 @@ module.exports.run = async (bot, msg, args)=> {
   if (msg.member.voice.channel) {
 
     const connection = await msg.member.voice.channel.join();
-    const dispatcher = connection.play('./AQSV_1.mp3');//, {volume: 2});
-
-    dispatcher.setVolume(2);
-
-     dispatcher.on('finish', () => {
-    });
-
-    const dispatcher = connection.play('./AQSV_2.mp3');//, {volume: 2});
+    const dispatcher = connection.play('./AQSV.mp3');//, {volume: 2});
 
     dispatcher.setVolume(2);
 
