637382f5bc09096620d1d16b180c20dbbd158c91
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-03 23:02:58 +0200

    AM

---
+++ commands/Mile_bise.js
@@ -0,0 +1,21 @@
+module.exports.run = async (bot, msg, args)=> {
+  if (msg.member.voice.channel) {
+
+    const connection = await msg.member.voice.channel.join();
+    const dispatcher = connection.play('./_______.mp3');//, {volume: 2});
+
+    dispatcher.setVolume(2);
+
+     dispatcher.on('finish', () => {
+       connection.disconnect();
+    });
+
+  } else {
+    msg.reply('Seules les dieux me donne des ordres!');
+  }
+}
+
+
+module.exports.help = {
+  name: 'bise'
+}
