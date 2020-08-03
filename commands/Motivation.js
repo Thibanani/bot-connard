92950c8304e394fa30534d31b69f22ef625014a6
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-03 23:13:52 +0200

    ho

--- commands/Motivation.js
+++ commands/Motivation.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot,msg,args) => {
-  const am_1 = require('../Photo/AM_1.png');
+  const am_1 = require('./AM_1.png');
   //const charlou = ["Hello","Helloooooooooooooooooooooooooooo","Wow '^'","'^'","C'est Génial!",":sheep:"];
   //msg.channel.send(charlou[Math.floor(Math.random() * (charlou.length - 0)+ 0)])
   msg.channel.send(am_1)
