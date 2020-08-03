40ac3f5a216cc5143252a9c7dc030a9392f55adf
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-04 00:15:02 +0200

    file(s)

--- commands/Motivation.js
+++ commands/Motivation.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot,msg,args) => {
   //const charlou = ["Hello","Helloooooooooooooooooooooooooooo","Wow '^'","'^'","C'est Génial!",":sheep:"];
   //msg.channel.send(charlou[Math.floor(Math.random() * (charlou.length - 0)+ 0)])
-  msg.channel.send("Votre motivation",{file: ['../Photo/AM_1.png']})
+  msg.channel.send("Votre motivation",{files: ['../Photo/AM_1.png']})
 }
 
 module.exports.help = {
--- commands/PDF.js
+++ commands/PDF.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot,msg,args) => {
-  msg.channel.send("PDF",{file: ['./Respirez.pdf']})
+  msg.channel.send("PDF",{files: ['./Respirez.pdf']})
 }
 
 module.exports.help = {
