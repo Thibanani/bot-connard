94fdfc83cc47dfb1db1833a8759eb94a72b85a2a
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-04 01:45:19 +0200

    modif aléatoire
    enfin bon
    Math.floor(Math.random() * MAX+1)

--- commands/Charmy.js
+++ commands/Charmy.js
@@ -1,7 +1,7 @@
 const Attachment = require('discord.js')
 module.exports.run = async (bot,msg,args) => {
   const charlou = ["Hello","Helloooooooooooooooooooooooooooo","Wow '^'","'^'","C'est Génial!",":sheep:","C'était Génial!"];
-  msg.channel.send(charlou[Math.floor(Math.random() * (charlou.length - 0)+ 0)])
+  msg.channel.send(charlou[Math.floor(Math.random() * charlou.length)])
 }
 
 module.exports.help = {
