e4160da8c82da9fe1b8646d8e2dd162162f0e076
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-04 00:28:30 +0200

    Update Motivation.js

--- commands/Motivation.js
+++ commands/Motivation.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot,msg,args) => {
-  //const AM = ["Hello","Helloooooooooooooooooooooooooooo","Wow '^'","'^'","C'est Génial!",":sheep:"];
-  //msg.channel.send(charlou[Math.floor(Math.random() * (charlou.length - 0)+ 0)])
+  
   msg.channel.send("Votre motivation",{files: ['./Photo/AM_'+ Math.floor(Math.random() * (30 - 0)+ 1) +'.png']})
+
 }
 
 module.exports.help = {
