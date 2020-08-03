8be1b7b7bc3d838b92a7e82276efd9dc7569023c
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-04 00:01:38 +0200

    Attachment

--- commands/Charmy.js
+++ commands/Charmy.js
@@ -1,3 +1,4 @@
+const Attachment = require('discord.js')
 module.exports.run = async (bot,msg,args) => {
   const charlou = ["Hello","Helloooooooooooooooooooooooooooo","Wow '^'","'^'","C'est Génial!",":sheep:"];
   msg.channel.send(charlou[Math.floor(Math.random() * (charlou.length - 0)+ 0)])
--- commands/PDF.js
+++ commands/PDF.js
@@ -1,3 +1,4 @@
+const Attachment = require('discord.js')
 module.exports.run = async (bot,msg,args) => {
   const respirez = new Attachment('./Respirez.pdf');
   msg.channel.send(respirez)
