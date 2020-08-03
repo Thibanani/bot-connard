03c96f88724528e84e43e94d10832e71969ef8c4
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-04 01:38:28 +0200

    Update PFC2.js

--- commands/PFC2.js
+++ commands/PFC2.js
@@ -4,14 +4,16 @@ module.exports.run = async (bot,msg,args) => {
   const pfc1 = [`Ha ouai, tu veux qu'on se règle`,`Viens chte bz`];
   const pfc = ['✊','✋','✌️'];
 
-  msg.channel.send(`${pfc1[Math.floor(Math.random() * (pfc1.length - 0)+ 1)]}`)
+  msg.channel.send(`${pfc1[Math.floor(Math.random() * ((pfc1.length-1 -0 +1))+ 0]}`)
   msg.channel.send(`On rappel pour les débiles : pierre :fist:    feuille :raised_hand:    ciseaux :v:`)
 
   const user = msg.author.id
   const filter = m => user === m.author.id;
   const collector = msg.channel.createMessageCollector(filter, { max: 1,time: 15000 });
 
-  symb = pfc[Math.floor(Math.random() * ((pfc1.length - 0)+ 1))]
+  symb = pfc[Math.floor(Math.random() * ((pfc.length-1 -0 +1))+ 0]
+
+
 
   collector.on('collect', m => {
   	console.log(`Collected :${m.content}`);
