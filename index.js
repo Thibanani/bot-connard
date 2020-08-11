4c13c42a6abf3bc253037f5e909c43c5064f0840
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-11 19:00:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on('message', async (msg) =>{
       msg.channel.send("I")
     }
     else {
-      msg.react(':two:');
+      msg.react('😄');
     //  msg.react(":u:");
       //msg.react(":i:");
     }
