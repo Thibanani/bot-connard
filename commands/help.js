b497ce7fc3f2713ba3b9176c93d9e7c18d661bb5
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-12 17:33:18 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -19,7 +19,7 @@ module.exports.run = async (bot,msg,args) => {
   	)
     .addFields(/*------------------- Pour Les commande texte -------------*/
   		{ name: '\u200B', value: '\u200B' },
-  		{ name: 'Mes meilleurs commande texte', value: "$Leboss : C'est toi le Boss ?", inline: true },
+  		{ name: 'Mes meilleurs commande texte', value: "$Leboss : C'est toi le Boss ?\n $Charlou : :sheep:", inline: true },
   		{ value: '$Charlou : :sheep:'},
       { value: '$PDF : Pour un max de rigolade'},
       { value: '$PFC : Teste moi dans un duel a mort'},
