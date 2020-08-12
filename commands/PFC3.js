d9badf4fe96ab7502216dfe792afc91a0438dbeb
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-12 16:12:27 +0200

    Update PFC3.js

--- commands/PFC3.js
+++ commands/PFC3.js
@@ -4,60 +4,20 @@ module.exports.run = async (bot,msg,args) => {
   const pfc1 = [`Ha ouai, tu veux qu'on se règle`,`Viens chte bz`];
   const pfc = ['✊','✋','✌️'];
 
-  msg.channel.send(`${pfc1[Math.floor(Math.random() * pfc1.length)]}`)
-  msg.channel.send(`On rappel pour les débiles : pierre :fist:    feuille :raised_hand:    ciseaux :v:`)
 
-  const user = msg.author.id
-  const filter = m => user === m.author.id;
-  const collector = msg.createReactionCollector(filter, { max: 1,time: 15000 });
+
+  const filter = m => bot.id === msg.author.id;
+  const collector = msg.channel.createMessageCollector(filter, { max: 1,time: 15000 });
 
   symb = pfc[Math.floor(Math.random() * pfc.length)]
 
 
 
   collector.on('collect', m => {
+    msg.channel.send(`${pfc1[Math.floor(Math.random() * pfc1.length)]}`)
+    msg.channel.send(`On rappel pour les débiles : pierre :fist:    feuille :raised_hand:    ciseaux :v:`)
+
   	console.log(`Collected :${m.content}`);
-    if (symb ==  '✊'){
-      msg.react(`✊`)
-      if (m.content == '✊'){
-        msg.channel.send(`Tu copie`)
-
-      }else if (m.content == '✋') {
-        msg.channel.send(`Vous êtes mon maitre`)
-
-      }else if (m.content == '✌️') {
-        msg.channel.send(`Bas alors on est nul!`)
-
-      }else{msg.channel.send(`Apprend à jouer avec $help`)}
-    }
-
-    else if (symb ==  '✋'){
-      msg.react(`✋`)
-      if (m.content == '✊'){
-        msg.channel.send(`Bas alors on est nul!`)
-
-      }else if (m.content == '✋') {
-        msg.channel.send(`Tu copie`)
-
-      }else if (m.content == '✌️') {
-        msg.channel.send(`Vous êtes mon maitre`)
-
-      }else{msg.channel.send(`Apprend à jouer avec $help`)}
-    }
-
-    else if (symb ==  '✌️'){
-      msg.react(`✌️`)
-      if (m.content == '✊'){
-        msg.channel.send(`Vous êtes mon maitre`)
-
-      }else if (m.content == '✋') {
-        msg.channel.send(`Bas alors on est nul!`)
-
-      }else if (m.content == '✌️') {
-        msg.channel.send(`Tu copie`)
-
-      }else{msg.channel.send(`Apprend à jouer avec $help`)}
-    }
   });
 
   collector.on('end', collected => {
