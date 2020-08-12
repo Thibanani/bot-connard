0ed6e69ec5e5a5f84d2d7462657adaa8825815bc
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-12 13:52:10 +0200

    Create PFC3.js

---
+++ commands/PFC3.js
@@ -0,0 +1,75 @@
+module.exports.run = async (bot,msg,args) => {
+  //const rep = require('/app/repartie.js');
+
+  const pfc1 = [`Ha ouai, tu veux qu'on se règle`,`Viens chte bz`];
+  const pfc = ['✊','✋','✌️'];
+
+  msg.channel.send(`${pfc1[Math.floor(Math.random() * pfc1.length)]}`)
+  msg.channel.send(`On rappel pour les débiles : pierre :fist:    feuille :raised_hand:    ciseaux :v:`)
+
+  const user = msg.author.id
+  const filter = m => user === m.author.id;
+  const collector = msg.channel.createReactionCollector(filter, { max: 1,time: 15000 });
+
+  symb = pfc[Math.floor(Math.random() * pfc.length)]
+
+
+
+  collector.on('collect', m => {
+  	console.log(`Collected :${m.content}`);
+    if (symb ==  '✊'){
+      m.react(`✊`)
+      if (m.content == '✊'){
+        msg.channel.send(`Tu copie`)
+
+      }else if (m.content == '✋') {
+        msg.channel.send(`Vous êtes mon maitre`)
+
+      }else if (m.content == '✌️') {
+        msg.channel.send(`Bas alors on est nul!`)
+
+      }else{msg.channel.send(`Apprend à jouer avec $help`)}
+    }
+
+    else if (symb ==  '✋'){
+      m.react(`✋`)
+      if (m.content == '✊'){
+        msg.channel.send(`Bas alors on est nul!`)
+
+      }else if (m.content == '✋') {
+        msg.channel.send(`Tu copie`)
+
+      }else if (m.content == '✌️') {
+        msg.channel.send(`Vous êtes mon maitre`)
+
+      }else{msg.channel.send(`Apprend à jouer avec $help`)}
+    }
+
+    else if (symb ==  '✌️'){
+      m.react(`✌️`)
+      if (m.content == '✊'){
+        msg.channel.send(`Vous êtes mon maitre`)
+
+      }else if (m.content == '✋') {
+        msg.channel.send(`Bas alors on est nul!`)
+
+      }else if (m.content == '✌️') {
+        msg.channel.send(`Tu copie`)
+
+      }else{msg.channel.send(`Apprend à jouer avec $help`)}
+    }
+  });
+
+  collector.on('end', collected => {
+    if (collected.size == 0)
+    {
+      msg.channel.send('Sale couard !')
+    }
+  });
+
+
+}
+
+module.exports.help = {
+  name: 'PFC2'
+}
