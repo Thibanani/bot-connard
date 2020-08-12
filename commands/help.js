2be6aeaa6428fb2c4d1fe6d6a4140814fa316903
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-12 17:39:54 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,22 +8,15 @@ module.exports.run = async (bot,msg,args) => {
   	.setDescription('tu trouveras toutes mes fonction ici et tâche de ne plus de déranger')
   	//.setThumbnail('https://i.imgur.com/wSTFkRM.png')
   	.addFields( /*------------------- Pour Les commande vocale -------------*/
-  		{ name: 'Mes meilleurs réactions vocale', value: '$c : Pour en mettre plein la geule à tes énemis', inline: true },
-  		{ value: '$q : Pour reçevoir ta question exixtancielle'},
-      { value: '$r : Pour reçevoir la réponse exixtancielle'},
-      { value: '$qr : Pour un max de mix'},
-      { value: '$qr : Pour un max de mix'},
-      { value: '$oof : Pour exprimer ton soulagement'},
-      { value: '$tg : Pour fermer le claper de tes adversaire'},
-      { value: '$topette : Juste la meilleur commande'},
+      { name: '\u200B', value: '\u200B' },
+  		{ name: 'Mes meilleurs réactions vocale',
+      value: '$c : Pour en mettre plein la geule à tes énemis \n $q : Pour reçevoir ta question exixtancielle \n $r : Pour reçevoir la réponse exixtancielle \n $qr : Pour un max de mix \n $qr : Pour un max de mix \n $oof : Pour exprimer ton soulagement \n $tg : Pour fermer le claper de tes adversaire \n $topette : Juste la meilleur commande', inline: true },
   	)
     .addFields(/*------------------- Pour Les commande texte -------------*/
   		{ name: '\u200B', value: '\u200B' },
-  		{ name: 'Mes meilleurs commande texte', value: "$Leboss : C'est toi le Boss ? \n
-       $Charlou : :sheep: \n
-       $PDF : Pour un max de rigolade \n
-       $PFC : Teste moi dans un duel a mort \n
-       $motiv : Juste la meilleur commande a utiliser sans modération", inline: true },
+  		{ name: 'Mes meilleurs commande texte',
+      value: "$Leboss : C'est toi le Boss ? \n $Charlou : :sheep: \n $PDF : Pour un max de rigolade \n $PFC : Teste moi dans un duel a mort \n $motiv : Juste la meilleur commande a utiliser sans modération", inline: true },
+      { name: '\u200B', value: '\u200B' },
   	)
   	.setTimestamp()
   	.setFooter("L'auteur c'est moi");
