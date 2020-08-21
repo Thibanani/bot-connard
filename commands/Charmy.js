fab9c52cc491689268c92763e493fb6c791f8f15
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-21 15:15:24 +0200

    Uptate charlou

--- commands/Charmy.js
+++ commands/Charmy.js
@@ -1,9 +1,17 @@
 const Attachment = require('discord.js')
 
 module.exports.run = async (bot,msg,args) => {
-  const charlou = ["Hello","Helloooooooooooooooooooooooooooo","Wow '^'","'^'","C'est Génial!",":sheep:","C'était Génial!","Coucou '^'" + {files: ['./Photo/AM_6.png']}];
 
-  msg.channel.send(charlou[Math.floor(Math.random() * charlou.length)])
+  const charlou = ["Hello","Helloooooooooooooooooooooooooooo","Wow '^'","'^'","C'est Génial!",":sheep:","C'était Génial!"];
+  var_temp = Math.floor(Math.random() * charlou.length+1)
+
+  if (var_temp == charlou.length) {
+    msg.channel.send("Coucou '^'",{files: ['./Photo/Coucou.png']})
+  }
+  else {
+    msg.channel.send(charlou[var_temp])
+  }
+
 }
 
 module.exports.help = {
