01b0fb251a0bacc3be3e2e6737472ba8f35e3c8d
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-27 18:21:11 +0200

    1

--- commands/Teasing.js
+++ commands/Teasing.js
@@ -2,7 +2,7 @@ const Duration = require('humanize-duration')
 module.exports.run = async (bot,msg,args) => {
   msg.channel.send("Ce soir en avant premiére se dévoile Charlou du 57")
 
-  const temps = Duration(Date.now() { units: ['h', 'm'], round: true});
+  const temps = Duration(Date.now(), { units: ['h', 'm'], round: true});
   msh.reply(temps)
 
 }
