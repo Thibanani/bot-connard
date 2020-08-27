4fbca421f1cc13b8f145f7d501a46fa36e1eacda
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-27 18:17:52 +0200

    yo

--- commands/Teasing.js
+++ commands/Teasing.js
@@ -1,11 +1,9 @@
-const datetime = require('datetime')
+const Duration = require('humanize-duration')
 module.exports.run = async (bot,msg,args) => {
   msg.channel.send("Ce soir en avant premiére se dévoile Charlou du 57")
 
-  now_utc = datetime.datetime.now(tz=pytz.utc)
-  now_luxembourg = now_utc.astimezone(pytz.timezone('Europe/Luxembourg'))
-  msg.channel.send(now_luxembourg);
-
+  const temps = Duration(Date.now() { units: ['h', 'm'], round: true});
+  msh.reply(temps)
 
 }
 
