cebe7c3bb0dffc4782b1056d219825306ba10389
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-28 00:07:57 +0200

    uptate

--- commands/Thib.js
+++ commands/Thib.js
@@ -1,5 +1,7 @@
 module.exports.run = async (bot,msg,args) => {
-    msg.channel.send("Thib est le pére de toutes choses vous le saviez ?")
+  const thib = ["Thib est le pére de toutes choses vous le saviez ?","Vous voulez dire Dieux ?","Juste le boss","Que voulez vous a mon créateur","C'est moi","C'est lui","Fun fact il fait 1m85"];
+
+    msg.channel.send(thib[Math.floor(Math.random() * thib.length)]);
 
 }
 
