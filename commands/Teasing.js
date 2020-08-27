14777ba0117dbee400c4e9eb49257206855d92a7
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-27 19:11:46 +0200

    merge

--- commands/Teasing.js
+++ commands/Teasing.js
@@ -1,6 +1,7 @@
 module.exports.run = async (bot,msg,args) => {
   msg.channel.send("Ce soir en avant premiére se dévoile Charlou du 57")
 
+<<<<<<< HEAD
   Date temps = new Date.now();
   Date evenement = new Date(2020,08,27).setHours(20);
   Date restant = new Date;
@@ -12,6 +13,10 @@ module.exports.run = async (bot,msg,args) => {
   const s = restant.getSeconds();
   
   msg.channel.send(h:m:s)
+=======
+  const temps = Date.now()
+  msg.channel.send(temps)
+>>>>>>> parent of 2f03b87... uptating
 
 }
 
