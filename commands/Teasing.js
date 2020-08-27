d2fdfc7e48b66a72efe7c80f0aa753fb1d4048e6
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-27 17:48:48 +0200

    f

--- commands/Teasing.js
+++ commands/Teasing.js
@@ -1,9 +1,8 @@
 module.exports.run = async (bot,msg,args) => {
   msg.channel.send("Ce soir en avant premiére se dévoile Charlou du 57")
 
-  ZoneId zoneId = ZoneId.of("UTC+2");
-  ZonedDateTime now = ZonedDateTime.now(); // Default time zone
-  msg.channel.send(now);
+  now_luxembourg = now_utc.astimezone(pytz.timezone('Europe/Luxembourg'))
+  msg.channel.send(now_luxembourg);
 
 
 }
