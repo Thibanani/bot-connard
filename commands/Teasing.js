91888f8ce8e9ffc0da589c0d4ca2dec40817f38c
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-27 17:10:50 +0200

    uptade with timer

--- commands/Teasing.js
+++ commands/Teasing.js
@@ -1,5 +1,11 @@
 module.exports.run = async (bot,msg,args) => {
   msg.channel.send("Ce soir en avant premiére se dévoile Charlou du 57")
+
+  ZoneId zoneId = ZoneId.of("UTC+2");
+  ZonedDateTime now = ZonedDateTime.now(); // Default time zone
+  msg.channel.send(now);
+
+
 }
 
 module.exports.help = {
