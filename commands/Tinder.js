186f94f0d9868621b542f5376e820da49462ff27
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-28 00:26:41 +0200

    yo

--- commands/Tinder.js
+++ commands/Tinder.js
@@ -1,7 +1,11 @@
 module.exports.run = async (bot,msg,args) => {
     msg.channel.send("Bas alors charmy")
-    msg.channel.send({files: ['./src/Photo/Conv_tinder_1.jpg']})
     msg.channel.send({files: ['./src/Photo/Charlou_tinder_2.png']})
+
+
+
+    msg.channel.send({files: ['./src/Photo/Conv_tinder_1.jpg']})
+
 
 }
 
