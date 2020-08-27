304dd8c19f2af7049c6b621e24bb8663bf205eef
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-27 17:54:52 +0200

    ui

--- commands/Teasing.js
+++ commands/Teasing.js
@@ -1,6 +1,8 @@
+const datetime = require('datetime')
 module.exports.run = async (bot,msg,args) => {
   msg.channel.send("Ce soir en avant premiére se dévoile Charlou du 57")
 
+  now_utc = datetime.datetime.now(tz=pytz.utc)
   now_luxembourg = now_utc.astimezone(pytz.timezone('Europe/Luxembourg'))
   msg.channel.send(now_luxembourg);
 
