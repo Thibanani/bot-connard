2cea8c5e571c94e25d7e6f2addf86f793d5b5258
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-08-27 19:29:23 +0200

    uptating

--- commands/Teasing.js
+++ commands/Teasing.js
@@ -1,16 +1,18 @@
 module.exports.run = async (bot,msg,args) => {
   msg.channel.send("Ce soir en avant premiére se dévoile Charlou du 57")
 
-  const temps = Date.now();
-  const evenement = Date(2020,08,27).setHours(20);
+  const temps = Date.getHours();
+  const evenement = Date.setHours(20);
 
-  restant = evenement - temps;
+  const h = temps.getHours();
+  const m = temps.getMinutes();
+  const s = temps.getSeconds();
 
-  const h = restant.getHours();
-  const m = restant.getMinutes();
-  const s = restant.getSeconds();
-
-  msg.channel.send(h +':'+ m +':'+ s)
+  msg.channel.Send(evenement)
+  msg.channel.Send(h);
+  msg.channel.Send(m);
+  msg.channel.Send(s);
+  //msg.channel.send(h +':'+ m +':'+ s)
 }
 
 module.exports.help = {
