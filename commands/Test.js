ef193f6e8538047a42ef3f719bcff35db655f7c1
Thibanani <63426672+Thibanani@users.noreply.github.com>
2020-09-19 22:40:31 +0200

    Update Test.js

--- commands/Test.js
+++ commands/Test.js
@@ -3,29 +3,28 @@ module.exports.run = async (bot, msg, args)=> {
   var newChannel;
   /*---------- Vérification de la permission ----------*/
   if(!msg.member.hasPermission("MOVE_MEMBERS")) {
-    return msg.channel.send(`**${msg.author.username}**, You do not have enough permission to use this command`)
+    return msg.channel.send(`**${msg.author.username}**, Ta crus t'étais dieux ?`)
   }
   if(!msg.guild.me.hasPermission("MOVE_MEMBERS")) {
-    return msg.channel.send(`**${msg.author.username}**, I do not have enough permission to use this command`)
+    return msg.channel.send(`**${msg.author.username}**, ... j'aimerai bien`)
   }
 
   /*---------- Vérification de l'id a kick ----------*/
   let target = msg.mentions.members.first();
 
    if(!target) {//Si l'id est correct
-     return msg.channel.send(`**${msg.author.username}**, Please mention the person who you want to kick`)
+     return msg.channel.send(`**${msg.author.username}**, Putain mais gros mentionne quelqu'un`)
    }
    if(target.id === msg.author.id) {//Si l'id est pas celui de l'auteur
-     return msg.channel.send(`**${msg.author.username}**, You can not kick yourself`)
+     return msg.channel.send(`**${msg.author.username}**, T'es maso ou quoi`)
     }
 
     key = "Le cachot";
     newChannel = msg.guild.channels.cache.find((val) =>  val.name === key ||val.id === key);
 
-    //target.edit(newChannel,"Parceque")
     target.voice.setChannel(newChannel)
     target.voice.setMute(true,"Oui")
-    msg.delete
+    msg.channel.bulkDelete(1)
 }
 
 
