acdd98dc1723bc4530ebfa090439106d1a4b7c94
Thibanani <63426672+Thibanani@users.noreply.github.com>
2020-09-19 23:11:55 +0200

    SPVNTR

---
+++ commands/SPVNTR.js
@@ -0,0 +1,23 @@
+module.exports.run = async (bot, msg, args)=> {
+  msg.channel.bulkDelete(1);
+
+  if (msg.member.voice.channel) {
+
+    const connection = await msg.member.voice.channel.join();
+    const dispatcher = connection.play('./src/Audio/SPVNTR.mp3');//, {volume: 2});
+
+    dispatcher.setVolume(0.5);
+
+     dispatcher.on('finish', () => {
+       connection.disconnect();
+    });
+
+  } else {
+    msg.reply('Seules les dieux me donne des ordres!');
+  }
+}
+
+
+module.exports.help = {
+  name: 'SPVNTR'
+}
