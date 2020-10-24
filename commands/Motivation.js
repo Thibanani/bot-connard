7b46d6774616bd7f74037e1e524107341553d399
Thibanani <63426672+Thibanani@users.noreply.github.com>
2020-10-24 17:35:53 +0200

    Update Motivation.js

--- commands/Motivation.js
+++ commands/Motivation.js
@@ -4,7 +4,9 @@ module.exports.run = async (bot,msg,args) => {
   }
   else if (args != ''){
     msg.channel.send("Vos motivation")
-    if (args >= 10) args = 10;
+    if (args >= 10) {
+      args = 10;
+    }
     for (i=0;i<=args-1;i++){
       msg.channel.send({files: ['./src/Photo/AM_'+ (Math.floor(Math.random() * 38) +1) +'.png']})
     }
