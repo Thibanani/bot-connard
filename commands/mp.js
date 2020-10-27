76a3ba420b5e7256f0683fb4ba65dc81aff9d541
Thibanani <63426672+Thibanani@users.noreply.github.com>
2020-10-27 19:11:38 +0100

    Update mp.js

--- commands/mp.js
+++ commands/mp.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, msg, args)=> {
   msg.channel.bulkDelete(1);
   mp = args.slice(1);
   m = mp.toString();
-  msg.channel.send(m)
+  msg.channel.send(`${m}`)
 
   /*---------- Vérification de l'id a mp ----------*/
   let target = msg.mentions.members.first();
