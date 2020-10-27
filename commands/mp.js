28086ec6564a7e92e04207377e605c41df53ed46
Thibanani <63426672+Thibanani@users.noreply.github.com>
2020-10-27 11:54:22 +0100

    Update mp.js

--- commands/mp.js
+++ commands/mp.js
@@ -8,9 +8,11 @@ module.exports.run = async (bot, msg, args)=> {
   else if(target.id === msg.author.id) {//Si l'id est pas celui de l'auteur
     msg.channel.send(`**${msg.author.username}**, T'es maso ou quoi`)
   }
-  else {  /*---------- code principale ----------*/
-    msg.channel.send(`Mentions : ${target.username}, arguments : ${args}, arguments 0 : ${args[0]}, arguments 1 : ${args[1]}`);
-    target.send(args[0])
+  else if (args [1] != ''){  /*---------- code principale ----------*/
+    target.send(`Message d'un anonyme : ${args[1]}`)
+  }
+  else {
+    msg.channel.send(`**${msg.author.username}**, Mais apprend à faire une commande`)
   }
 }
 module.exports.help = {
