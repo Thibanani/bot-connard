f14c97330085f26c72f8fce2235d0dc0aacb13da
Thibanani <63426672+Thibanani@users.noreply.github.com>
2020-10-27 11:25:04 +0100

    mp + jail

---
+++ commands/mp.js
@@ -0,0 +1,20 @@
+module.exports.run = async (bot, msg, args)=> {
+  /*---------- Vérification de l'id a mp ----------*/
+  let target = msg.mentions.members.first();
+
+  if(!target) {//Si l'id est correct
+    msg.channel.send(`**${msg.author.username}**, Putain mais gros mentionne quelqu'un`)
+  }
+  else if(target.id === msg.author.id) {//Si l'id est pas celui de l'auteur
+    msg.channel.send(`**${msg.author.username}**, T'es maso ou quoi`)
+  }
+  else {  /*---------- code principale ----------*/
+    target.send(args[1]);
+  }
+
+
+
+
+module.exports.help = {
+  name: 'mp'
+}
