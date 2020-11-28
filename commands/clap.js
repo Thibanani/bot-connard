de598ba65fed17837ed076fd8ddd695e369c46ad
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-11-28 17:45:40 +0100

    Update clap.js

--- commands/clap.js
+++ commands/clap.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, msg, args)=> {
   if (msg.member.voice.channel) {
 
     const connection = await msg.member.voice.channel.join();
-    const dispatcher = connection.play('./src/Audio/jaccepte_pas.mp3');//, {volume: 2});
+    const dispatcher = connection.play('./src/Audio/clap.mp3');//, {volume: 2});
 
     dispatcher.setVolume(0.5);
 
@@ -17,5 +17,5 @@ module.exports.run = async (bot, msg, args)=> {
 
 
 module.exports.help = {
-  name: 'jacceptepas'
+  name: 'clap'
 }
