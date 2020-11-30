56bb87e5e7fb0890abc3c191644ada03ad9a8451
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-11-30 23:32:00 +0100

    Update Puissance_4.js

--- commands/Puissance_4.js
+++ commands/Puissance_4.js
@@ -16,13 +16,15 @@ module.exports.run = async (bot, msg, args)=> {
   //coniguration tab_grille[ligne][colonne]
 
   // ---------- Affichage de la grille
-  msg.channel.send(`<1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣>`)
   for (i=0;i<=tab_grille.length;i++){
-    msg.channel.send(`${tab_grille[i][1]}${tab_grille[i][2]}${tab_grille[i][3]}${tab_grille[i][4]}${tab_grille[i][5]}${tab_grille[i][6]}|`)
+    msg.channel.send(`${tab_grille[i]}${tab_grille[i]}${tab_grille[i]}${tab_grille[i]}${tab_grille[i]}${tab_grille[i]}|`)
   }
   do {
     // ---------- Collecteur de la colonne ou jettons a placer
-
+    msg.channel.send(`<1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣>`)
+    //collecteur pour être sure que le message est envoyée
+    const filter_0 = m => m.author.id === bot.user.id && m.content === `On rappelle pour les débiles :  pierre :fist:    feuille :raised_hand:    ciseaux :v:`;
+    const collector_0 = channel_1.createMessageCollector(filter_0, { max: 1,time: 15000 });
 
     // ---------- Affichage de la grille
 
