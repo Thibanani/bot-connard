bc2891a97f5a686bc49fa903c892b81af75c6149
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-11-30 23:26:34 +0100

    Update Puissance_4.js

--- commands/Puissance_4.js
+++ commands/Puissance_4.js
@@ -12,10 +12,11 @@ module.exports.run = async (bot, msg, args)=> {
 
   var fin = 0, joueur_prec, gagant,i;
   const tab_joueur = [joueur_1,joueur_2];
-  var tab_grille = [['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      ']];
+  var tab_grille = [['|      ','|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      ','|      ']];
+  //coniguration tab_grille[ligne][colonne]
 
   // ---------- Affichage de la grille
-  msg.channel.send(`:one::two::three::four::five::six:`)
+  msg.channel.send(`1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣`)
   for (i=0;i<=tab_grille.length;i++){
     msg.channel.send(`${tab_grille[i][0]}${tab_grille[i][1]}${tab_grille[i][2]}${tab_grille[i][3]}${tab_grille[i][4]}${tab_grille[i][5]}|`)
   }
