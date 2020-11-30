9f8b26c0fccede9c2cfc17841f5ed009e024b4ff
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2020-11-30 23:03:46 +0100

    Update Puissance_4.js

--- commands/Puissance_4.js
+++ commands/Puissance_4.js
@@ -9,10 +9,14 @@ module.exports.run = async (bot, msg, args)=> {
 
 
 
-  var fin = 0, joueur_prec, gagant;
+  var fin = 0, joueur_prec, gagant,i;
   const tab_joueur = [joueur_1,joueur_2];
-  var tab_grille = [['','','','','',''],['','','','','',''],['','','','','',''],['','','','','',''],['','','','','',''],['','','','','',''],['','','','','','']];
+  var tab_grille = [['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      '],['|      ','|      ','|      ','|      ','|      ','|      ']];
+
   // ---------- Affichage de la grille
+  for (i=0;i<=tab_grille.length;i++){
+    msg.channel.send(`${tab_grille[i][0]}${tab_grille[i][1]}${tab_grille[i][2]}${tab_grille[i][3]}${tab_grille[i][4]}${tab_grille[i][5]}|`)
+  }
   msg.channel.send()
   do {
     // ---------- Collecteur de la colonne ou jettons a placer
