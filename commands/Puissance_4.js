769fc7c5815ad75c734eee04f32f80e20e7bca06
Thibanani <63426672+Thibanani@users.noreply.github.com>
2020-12-01 23:51:52 +0100

    Update Puissance_4.js

--- commands/Puissance_4.js
+++ commands/Puissance_4.js
@@ -19,24 +19,8 @@ module.exports.run = async (bot, msg, args)=> {
                     ['|      ','|      ','|      ','|      ','|      ','|      ','|      '],
                     ['|      ','|      ','|      ','|      ','|      ','|      ','|      '],
                     ['|      ','|      ','|      ','|      ','|      ','|      ','|      '] ];
-
-  //Piste a dévelloper
-  /*var tab_grille = [
-    {c1: '|      ',c2: '|      ',c3: '|      ',c4: '|      ',c5: '|      ',c6: '|      '},//1
-    {c1: '|      ',c2: '|      ',c3: '|      ',c4: '|      ',c5: '|      ',c6: '|      '},//2
-    {c1: '|      ',c2: '|      ',c3: '|      ',c4: '|      ',c5: '|      ',c6: '|      '},//3
-    {c1: '|      ',c2: '|      ',c3: '|      ',c4: '|      ',c5: '|      ',c6: '|      '},//4
-    {c1: '|      ',c2: '|      ',c3: '|      ',c4: '|      ',c5: '|      ',c6: '|      '},//5
-    {c1: '|      ',c2: '|      ',c3: '|      ',c4: '|      ',c5: '|      ',c6: '|      '},//6
-    {c1: '|      ',c2: '|      ',c3: '|      ',c4: '|      ',c5: '|      ',c6: '|      '}//7
-];
-
-//https://stackoverflow.com/questions/48909283/discord-js-how-do-i-set-a-value-to-an-array-string
-*/
-
-
-
   //coniguration tab_grille[ligne][colonne]
+
 
   // ---------- Affichage de la grille
   for (i=0;i<=tab_grille.length-1;i++){
