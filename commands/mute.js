bc9d4db2ddcad64d21e25596fb4e452c3aaa8f5e
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2021-01-11 23:35:02 +0100

    g

--- commands/mute.js
+++ commands/mute.js
@@ -1,9 +1,6 @@
 module.exports.run = async (bot, msg, args)=> {
   msg.channel.bulkDelete(1);
 
-  var newChannel;
-
-
   /*---------- Vérification de la permission ----------*/
   if(!msg.member.hasPermission("MUTE_MEMBERS")) {
     return msg.channel.send(`**${msg.author.username}**, Ta crus t'étais dieux ?`)
--- commands/tictacto/un_mute.js
+++ commands/tictacto/un_mute.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, msg, args)=> {
   msg.channel.bulkDelete(1);
 
 
-
+  console.log("YO");
   /*---------- Vérification de la permission ----------*/
   if(!msg.member.hasPermission("MUTE_MEMBERS")) {
     return msg.channel.send(`**${msg.author.username}**, Ta crus t'étais dieux ?`)
@@ -14,6 +14,7 @@ module.exports.run = async (bot, msg, args)=> {
   let channel = msg.member.voice.channel;
   for (let member of channel.members) {
       member[1].voice.setMute(false)
+      console.log(member[1]);
   }
 }
 
