cacc3cac066a455206ee55de97c1146d2a17430d
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2021-01-11 23:13:00 +0100

    c

--- commands/tictacto/un_mute.js
+++ commands/tictacto/un_mute.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, msg, args)=> {
     return msg.channel.send(`**${msg.author.username}**, ... j'aimerai bien`)
   }
 
-  let channel = msg.author.voiceChannel;
+  let channel = msg.author.voice.channel;
   for (let member of channel.members) {
       member[1].setMute(true)
   }
