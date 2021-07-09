78d4500644924cba77fc12eddac0eaef2b43f512
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2021-07-09 21:28:46 +0200

    Update jaune.js

--- commands/jaune.js
+++ commands/jaune.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, msg, args)=> {
     if (cible) {//Si la cible existe que se soit l'autheur ou un autre membre
       msg.channel.bulkDelete(1);
       const connection = await cible.voice.channel.join();
-      const dispatcher = connection.play(audio);//, {volume: 2});
+      const dispatcher = connection.play(ytdl(audio, { quality: 'highestaudio' }));//, {volume: 2});
 
       dispatcher.setVolume(volume);
 
