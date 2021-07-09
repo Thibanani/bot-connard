ca3e9cf9862b2d3499b2c94cc96b63b4c2167e03
Thibault BANSIERE <thibault.bansiere@etu.univ-nantes.fr>
2021-07-09 21:24:18 +0200

    Create jaune.js

---
+++ commands/jaune.js
@@ -0,0 +1,39 @@
+module.exports.run = async (bot, msg, args)=> {
+  let cible = msg.mentions.members.first();
+  const audio = 'https://www.youtube.com/watch?v=wNch1OqYgIM'
+  const volume = 0.25
+  //console.log(cible);
+
+  if (args != ''){
+    if (cible) {//Si la cible existe que se soit l'autheur ou un autre membre
+      msg.channel.bulkDelete(1);
+      const connection = await cible.voice.channel.join();
+      const dispatcher = connection.play(audio);//, {volume: 2});
+
+      dispatcher.setVolume(volume);
+
+      dispatcher.on('finish', () => {
+         connection.disconnect();
+      });
+
+    } else {
+      msg.reply('Il existe pas ! Connard');
+    }
+
+  }else {
+
+    const connection = await msg.member.voice.channel.join();
+    const dispatcher = connection.play(audio);//, {volume: 2});
+
+    dispatcher.setVolume(volume);
+
+    dispatcher.on('finish', () => {
+       connection.disconnect();
+    });
+  }
+}
+
+
+module.exports.help = {
+  name: 'jaune'
+}
